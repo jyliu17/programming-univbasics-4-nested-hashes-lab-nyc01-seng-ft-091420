55e0f8ee9839b708af94fd093fa19e40cd62aad0
Johnson Liu <jyliu17@gmail.com>
2020-08-27 22:20:17 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,6 +1,7 @@
 
 def hopper
-	programmer_hash = 
+  
+  	programmer_hash = 
  		{
       :grace_hopper => {
         :known_for => "COBOL",
@@ -14,7 +15,8 @@ def hopper
         :known_for => "Unix",
         :languages => ["C"]
       }
-    }
+          }
+hopper = programmer_hash[:grace_hopper]
 
 end
 
@@ -36,7 +38,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+ programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +57,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+ programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -81,6 +83,8 @@ def adding_matz
         :languages => ["C"]
       }
     }
+programmer_hash[:yukihiro_matsumoto]{:known_for => "Ruby", :languages => ["LISP", "C"]}
+programmer_hash
 
 end
 
@@ -103,7 +107,7 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for] = "GUI"
 end
 
 def adding_to_dennis
